1d09f6a74e82923a47f9e09600c856ca95085582
Sanzhar <SanzharAmirzhan@users.noreply.github.com>
2017-04-25 17:36:07 +0600

    new

--- auth_app/views.py
+++ auth_app/views.py
@@ -1,6 +1,9 @@
-from django.http import JsonResponse, HttpResponse
+from django.http import HttpResponse, JsonResponse
 from rest_framework import status
+from rest_framework.decorators import permission_classes, authentication_classes, api_view
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.parsers import JSONParser
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 from .serializers import UserSerializer
 
@@ -14,4 +17,14 @@ def register(request):
             return JsonResponse(serialized.data, status=status.HTTP_200_OK)
         else:
             return JsonResponse(serialized.errors, status=status.HTTP_400_BAD_REQUEST)
+    return HttpResponse(status=400)
+
+@api_view(['GET'])
+@authentication_classes([JSONWebTokenAuthentication, ])
+@permission_classes([IsAuthenticated, ])
+def getInfo(request):
+    if(request.method == 'GET'):
+        user = request.user
+        serialized = UserSerializer(user)
+        return JsonResponse(serialized.data, status=status.HTTP_200_OK)
     return HttpResponse(status=400)
