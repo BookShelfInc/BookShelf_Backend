a58bdb080e17d665a6f45b304f1fe07a7fe3119f
Sanzhar <SanzharAmirzhan@users.noreply.github.com>
2017-04-28 09:30:55 +0600

    bazaar

--- bazaar_app/serializers.py
+++ bazaar_app/serializers.py
@@ -2,7 +2,12 @@ from rest_framework import serializers
 
 from .models import BazaarBook
 
+from auth_app.serializers import UserSerializer
+from book_app.serializers import BookSerializer
+
 class BazaarBookSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    book = BookSerializer()
     class Meta:
         model = BazaarBook
         fields = (
